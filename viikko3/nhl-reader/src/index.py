05c83f9a4a309f2d0db99a687e6270f3559fd8c4
Topi Pärssinen <parssinentopi@gmail.com>
2022-11-21 15:54:29 +0200

    3.1

---
+++ viikko3/nhl-reader/src/index.py
@@ -0,0 +1,30 @@
+import requests
+from player import Player
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2021-22/players"
+    response = requests.get(url).json()
+
+    players = []
+
+    for player_dict in response:
+        player = Player(
+            player_dict['name'],
+            player_dict['nationality'],
+            player_dict['assists'],
+            player_dict['goals'],
+            player_dict['penalties'],
+            player_dict['team'],
+            player_dict['games']
+        )
+
+        players.append(player)
+
+    print("Oliot:")
+    for player in players:
+        #print(player)
+        if player.nationality == "FIN":
+            print (player)
+
+if __name__ == "__main__":
+    main()
---
+++ viikko3/nhl-reader/src/player.py
@@ -0,0 +1,13 @@
+class Player:
+    def __init__(self, name, nationality, assists, goals, penalties, team, games):
+        self.name = name
+        self.nationality=nationality
+        self.assists=assists
+        self.goals=goals
+        self.penalties=penalties
+        self.team=team
+        self.games=games
+
+    def __str__(self):
+        loru= f"{self.name} {self.nationality}, {str(self.goals)} + {str(self.assists)}, {str(self.games)} peliä"
+        return loru
